a74935b896c5ced86d4ea370a4ab79859dd8f8a3
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-08-23 14:35:48 -0500

    completed routes for CRUD operations

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    })
+    res.status(200).json(productData);
+    if (!productData) {
+      res.status(404).json({ message: 'Product could not be found by this id'})
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
@@ -91,6 +110,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Product could not be found by that id'})
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
